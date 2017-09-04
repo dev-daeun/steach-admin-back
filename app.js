b29c004c0ffb28fafc134736ed7aadbf7bb4db75
kde6260 <kde6260@gmail.com>
2017-09-05 02:14:11 +0900

    refactor: update student controller, add logic that cancels assignment, add custom error

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var app = express();
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -13,24 +13,23 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+
+
 app.use(function(req, res, next){
   if(req.url!='/favicon-32x32.png') next();
 });
+// app.use(methodOverride('_method'))
 app.use('/', require('./controller/index'));
 app.use('/teacher', require('./controller/teacher'));
 app.use('/student', require('./controller/student'));
 app.use('/assignment', require('./controller/assignment'));
 app.use(express.static(__dirname + '/public'));
 
-app.use(function(err, req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
 app.use(function(err, req, res, next) {
   console.log(err);
-  res.sendStatus(err.status || 500);
+  res.status(err.status || 500).send(err.result);
 });
 
 var server = http.createServer(app);
---
+++ model/expectation.js
@@ -0,0 +1,15 @@
+class Expectation {}
+
+
+Expectation.updateById = function(connection, record, id){
+    return new Promise((resolve, reject) => {
+        connection.query('update expectation set ? where id = ?', [record, id], (err) => {
+            console.log('expect done');
+            if(err) reject([err, connection]);
+            else resolve(connection);
+        });
+    });
+};
+
+
+module.exports = Expectation;
