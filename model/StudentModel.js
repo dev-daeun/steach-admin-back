0eba76a43b056dd465b0e42940dc9494ea2d2d02
kde6260 <kde6260@gmail.com>
2017-09-12 16:30:34 +0900

    퇴원학생 조회 ejs 적용 및 추후 수정

--- model/StudentModel.js
+++ model/StudentModel.js
@@ -17,45 +17,6 @@ const Student = bookshelf.Model.extend({
         },
         course: function(){
             return this.hasMany(Course);
-        },
-    },
-    {  
-        selectRetired: function(){
-            return knex.raw(`
-                SELECT ATD.attendance, INF.* 
-                FROM   ((SELECT attendance, 
-                                course_id 
-                         FROM   lesson) AS ATD 
-                        RIGHT JOIN (SELECT TC.teacher_name, 
-                                           TC.course_id, 
-                                           STU.* 
-                                    FROM   (SELECT c.student_id, 
-                                                   c.id   AS course_id, 
-                                                   t.NAME AS teacher_name 
-                                            FROM   teacher t, 
-                                                   course c 
-                                            WHERE  c.teacher_id = t.id) AS TC 
-                                            RIGHT JOIN (SELECT s.id AS student_id, 
-                                                               e.id AS expect_id, 
-                                                               assign_status, 
-                                                               fail_reason, 
-                                                               subject, 
-                                                               school_name, 
-                                                               grade, 
-                                                               concat(address1, ' ', address2) AS address, 
-                                                               NAME AS student_name, 
-                                                               called_consultant, 
-                                                               visited_consultant, 
-                                                               class_form, 
-                                                               fee, 
-                                                               calling_day, 
-                                                               visiting_day, 
-                                                               first_date 
-                                            FROM   student s, expectation e 
-                                            WHERE  s.id = e.student_id 
-                                                   AND e.assign_status = 0) AS STU 
-                                            ON STU.student_id = TC.student_id) AS INF 
-                          ON ATD.course_id = INF.course_id )`);
         }
     }
 );
