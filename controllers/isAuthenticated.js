289b7a5e7acd13c9e07880b907b6bf957126f546
kde6260 <kde6260@gmail.com>
2017-10-27 00:14:19 +0900

    로그아웃 구현

--- controllers/isAuthenticated.js
+++ controllers/isAuthenticated.js
@@ -1,8 +1,10 @@
 const Jwt = require('../utils/jwt');
 const CustomError = require('../libs/customError');
+
+
 module.exports = function(req, res, next){
-    if(!req.cookies.token) return res.redirect('/signin');
-    Jwt.verify(req.cookies.token)
+    if(!req.cookies.SteachToken) return res.redirect('/signin');
+    Jwt.verify(req.cookies.SteachToken)
     .then(decoded => {
         if(parseInt(decoded)!==req.session.passport.user) 
             return next(new CustomError(401, '유효하지 않은 사용자 정보입니다.'));
--- controllers/signController.js
+++ controllers/signController.js
@@ -18,7 +18,7 @@ router.post('/signin', function(req, res, next){
             if(err) return next(new CustomError(500, err.message || err));
             Jwt.sign(String(user))
             .then(newToken => {
-                res.status(200).cookie('token', newToken).send(true);
+                res.status(200).cookie('SteachToken', newToken).send(true);
             })
             .catch(signError => {
                 return next(new CustomError(500, err.message || err));
@@ -42,14 +42,26 @@ router.get('/signin', function(req, res, next){
     });
 });
 
-
-router.use(require('./isAuthenticated'));
-
 router.get('/', function(req, res, next){
    res.redirect('/dashboard'); 
 });
 
 
+router.use(require('./isAuthenticated'));
+
+
+/* 로그아웃 */
+router.get('/signout', function(req, res, next){
+    req.session.destroy(err => {
+        if(err) next(new CustomError(500, err.message || err));
+        req.user = null;
+        res.clearCookie('SteachToken')
+           .clearCookie('connect.sid')
+           .status(200)
+           .redirect('/signin');
+    });
+   
+});
 
 
 module.exports = router;
